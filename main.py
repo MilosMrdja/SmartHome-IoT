5bd487b2257a952195c59c215ce7c3203aae83c5
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2026-02-21 22:18:21 +0100

    feat: add db to PI 1

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ if __name__ == "__main__":
     # 2. Pokretanje senzora za odredjeni PI
     if settings["device_info"]["pi_id"] == "PI1" :
         #run_ds1(settings['DS1'], threads, stop_event, device_info) # vezbe 2
-        #run_dl(settings['DL'], threads, stop_event, device_info) # vezbe 1
-        #run_dus1(settings['DUS1'], threads, stop_event, device_info) # vezbe 3, jedan provodin 330, dva redna od 220
+        run_dl(settings['DL'], threads, stop_event, device_info) # vezbe 1
+        run_dus1(settings['DUS1'], threads, stop_event, device_info) # vezbe 3, jedan provodin 330, dva redna od 220
         #run_db(settings['DB'], threads, stop_event, device_info) # vezbe 2
         #run_dpir1(settings['DPIR1'], threads, stop_event, device_info) # vezbe 2
         run_dms(settings['DMS'], threads, stop_event, device_info) # vezbe 4
