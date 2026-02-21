f13c8975506f65e6af0a2266227a2692bf5402c5
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2026-02-21 22:15:48 +0100

    feat: PI 1

--- server/app.py
+++ server/app.py
@@ -1,27 +1,32 @@
-from flask import Flask
+import threading
+
+from flask import Flask, render_template
 import paho.mqtt.client as mqtt
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import json
+from config.config import MQTT_BROKER, MQTT_TOPIC,INFLUX_URL, INFLUX_TOKEN, INFLUX_ORG, INFLUX_BUCKET
 
 app = Flask(__name__)
 
-# --- KONFIGURACIJA ---
-MQTT_BROKER = "192.168.107.129"   # LAN IP Windows mašine ili 'mqtt5' ako je Docker
-MQTT_TOPIC = "iot/+/batch"
 
-INFLUX_URL = "http://localhost:8086"
-INFLUX_TOKEN = "-TgOl9wj2NCpJU5BdiEcojewgMRhVM3nfaMAg-RrGCWEnLyUuex2gTOO-hru_t_ruvJAJZ3lFg7x7Hmpddxeyw=="
-INFLUX_ORG = "docs"
-INFLUX_BUCKET = "home"
 
 # InfluxDB setup
 influx_client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
 write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
+# VAR
+mqtt_connected = False
+
 # --- MQTT CALLBACKS ---
 def on_connect(client, userdata, flags, rc):
-    print("Connected to MQTT Broker with result code", rc)
+    global mqtt_connected
+    if rc == 0:
+        mqtt_connected = True
+        print("Connected to MQTT Broker")
+    else:
+        mqtt_connected = False
+        print("Connection failed")
     client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
@@ -39,7 +44,7 @@ def on_message(client, userdata, msg):
                 .field("value", data['value']) #TODO izmeniti za dms
             )
             write_api.write(bucket=INFLUX_BUCKET, record=point)
-            print(f"Saved to Influx: {data}")
+            print(f"Saved to Influx: {data}\n")
     except Exception as e:
         print("Error writing to Influx:", e)
 
@@ -48,5 +53,30 @@ mqtt_client = mqtt.Client(protocol=mqtt.MQTTv311)
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
-mqtt_client.connect(MQTT_BROKER, 1883, 60)
-mqtt_client.loop_forever()
+
+@app.route('/')
+def index():
+    status_text = "Online" if mqtt_connected else "Offline"
+    status_color = "green" if mqtt_connected else "red"
+    mqtt_info = "Povezan" if mqtt_connected else "Nije povezan"
+
+    return render_template('index.html',
+                           status=status_text,
+                           color=status_color,
+                           mqtt_status=mqtt_info,
+                           mqtt_broker=MQTT_BROKER,
+                           org_name=INFLUX_ORG)
+
+
+def run_mqtt():
+    mqtt_client.connect(MQTT_BROKER, 1883, 60)
+    mqtt_client.loop_forever()
+
+
+if __name__ == "__main__":
+
+    thread = threading.Thread(target=run_mqtt)
+    thread.daemon = True
+    thread.start()
+
+    app.run(debug=True, port=5000)
---
+++ server/config/config.py
@@ -0,0 +1,8 @@
+# --- KONFIGURACIJA ---
+MQTT_BROKER = "192.168.0.102"   # LAN IP Windows mašine ili 'mqtt5' ako je Docker
+MQTT_TOPIC = "iot/+/batch"
+
+INFLUX_URL = "http://localhost:8086"
+INFLUX_TOKEN = "70yNZKPwx1aKTvl0GMICWtKNFYjIAC02sRnRxC9yq5dMhUQY0str4hwDJEfcqDHMZhWnPzCPgs53WY354mzN9w=="
+INFLUX_ORG = "docs"
+INFLUX_BUCKET = "home"
